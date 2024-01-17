7e34be06b11feaa49a931a63a730e866b2eef0c8
Soniamy <marianna.marzullo@gmail.com>
2024-01-17 14:56:39 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script>
+//Importazione del componente
+import AppHeader from './components/AppHeader.vue';
+export default {
+  data() {
+    return { count: 0 };
+  },
+  //Dichiarazione del componente
+  components: {
+    AppHeader,
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <!--Utilizzo del componente-->
+    <AppHeader />
+    <h1>Hello World</h1>
+    <p>
+      {{ count }}
+    </p>
+    <button @click="count++">Incrementa count</button>
+    <p class="subtitle">A primo impatto sembra molto figo !</p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+  font-size: 5em;
+  color: blueviolet;
+}
+p {
+  text-align: center;
+}
+button {
+  margin-left: 655px;
+  margin-top: 10px;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return { count: 0 };
+  },
+  template: `<div>count is {{ count }}</div>`,
+};
+</script>
+
+<template>
+  <header>
+    <h1 class="main-title">{{ title }} Ciao ragazzi</h1>
+  </header>
+</template>
+
+<style scoped>
+header {
+  padding: 20px;
+  background-color: rgb(182, 130, 188);
+}
+</style>
